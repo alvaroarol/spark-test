4266b7d494c11c288cddf3800b26113634dc1cc2
Alvaro Arroyo Olalla <alvaro.arol@gmail.com>
2021-12-01 10:26:44 +0100

    First commit

---
+++ src/main/java/com/ses/spark/SparkOnKafkaTest.java
@@ -0,0 +1,74 @@
+package com.ses.spark;
+
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import static org.apache.spark.sql.functions.from_json;
+import static org.apache.spark.sql.functions.explode;
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.window;
+import org.apache.spark.sql.types.ArrayType;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.streaming.StreamingQueryException;
+import java.util.concurrent.TimeoutException;
+
+public class SparkOnKafkaTest {
+
+	public static void main(String[] args) throws StreamingQueryException, TimeoutException {
+		
+		// Schema of the retrieved Kafka records (array of records)
+		StructType schema = new StructType(
+			new StructField[] {
+				new StructField("id", DataTypes.StringType, false, Metadata.empty()),
+				new StructField("timeStamp", DataTypes.TimestampType, false, Metadata.empty()),
+				new StructField("valueToAverage", DataTypes.FloatType, false, Metadata.empty())
+			}
+		);
+		ArrayType arrSchema = new ArrayType(schema, false);
+		
+		// Create Spark session
+		SparkSession spark = SparkSession
+				.builder()
+				.appName("SparkOnKafkaTest")
+				.getOrCreate();
+		
+		// Subscribe to the Kafka topic 'rawValues'
+		Dataset<Row> rawData = spark
+			.readStream()
+			.format("kafka")
+			.option("kafka.bootstrap.servers", "localhost:9092")
+			.option("subscribe", "rawValues")
+			.option("startingOffsets", "earliest")
+			.load();
+		
+		// Get the records (value) from the retrieved message
+		Dataset<Row> jsonData = rawData.selectExpr("CAST(value AS STRING)");
+		
+		// Prepare columns
+		Column explodedCols = explode(from_json(col("value"), arrSchema));
+		
+		// Structure retrieved records in columns
+		Dataset<Row> records = jsonData.select(explodedCols).select("col.*");
+		records.printSchema();
+		
+		// Average 'valueToAverage' per ID
+		Dataset<Row> averageRecords = records.groupBy("id").avg("valueToAverage");
+
+		// Write back to kafka (only value, no key)
+		averageRecords.selectExpr("to_json(struct(*)) AS value")
+			.writeStream()
+			.format("kafka")
+			.outputMode("update") // Don't send all the averages for all IDs, only the ones with an updated average
+			.option("kafka.bootstrap.servers", "localhost:9092")
+			.option("topic", "averageValues")
+			.option("checkpointLocation", "/tmp/spark-on-kafka-test/checkpoint")
+			.start()
+			.awaitTermination();
+
+	}
+
+}
